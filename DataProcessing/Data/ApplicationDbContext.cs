e136499f4b282d8a78092f282baf9bdd07d0e5d9
HuyJun2305 <125681217+HuyJun2305@users.noreply.github.com>
2024-09-18 13:57:34 +0700

    Dbset

--- DataProcessing/Data/ApplicationDbContext.cs
+++ DataProcessing/Data/ApplicationDbContext.cs
@@ -16,5 +16,49 @@ namespace DataProcessing.Data
                 : base(options)
         {
         }
+
+        public DbSet<ApplicationUser> Accounts { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartDetail> CartDetails { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<OrderHistory> OrderHistories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductDetail> ProductDetails { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+        public DbSet<Size> Sizes { get; set; }
+        public DbSet<Sole> Soles { get; set; }
+        public DbSet<Voucher> Vouchers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=HUYJUN\\SQLEXPRESS;Initial Catalog=DUANTHUCTAP;Integrated Security=True;Trust Server Certificate=True");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // SeedData for Account
+            var adminRoleId = Guid.NewGuid();
+            var customerRoleId = Guid.NewGuid();
+            var employeeRoleId = Guid.NewGuid();
+            var guestRoleId = Guid.NewGuid();
+
+            modelBuilder.Entity<IdentityRole<Guid>>().HasData
+                (
+                    new IdentityRole<Guid>
+                    {
+                        Id = adminRoleId,
+                        Name = "Admin",
+                        NormalizedName = "ADMIN"
+                    }
+                );
+        }
+
     }
 }
