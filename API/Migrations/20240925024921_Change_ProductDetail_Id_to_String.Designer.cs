93977274730e537be2cd5db19f3cf002fa793327
giaptd1610 <giaptd.work@gmail.com>
2024-09-25 10:37:21 +0700

    Sửa Id của ProductDetails từ Guid sang string, và các khoá phụ trong bảng CartDetail, OrderDetail. Thêm "Xác thực có tồn tại trong sản phẩm nào đó" vào phần Delete của Sole, Brand, Material, Category.

--- API/Migrations/20240925024921_Change_ProductDetail_Id_to_String.Designer.cs
+++ API/Migrations/20240925024921_Change_ProductDetail_Id_to_String.Designer.cs
@@ -4,6 +4,7 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240925024921_Change_ProductDetail_Id_to_String")]
+    partial class Change_ProductDetail_Id_to_String
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
