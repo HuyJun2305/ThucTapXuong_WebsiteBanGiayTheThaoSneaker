edb82467d9f2db967018ad0997e5385368396423
thanhdz2003 <115550700+thanhdz2003@users.noreply.github.com>
2024-09-25 09:46:45 +0700

    all

---
+++ API/Repositories/VoucherRepos.cs
@@ -0,0 +1,48 @@
+﻿using API.Data;
+using API.IRepositories;
+using DataProcessing.Models;
+
+namespace API.Repositories
+{
+    public class VoucherRepos : IVoucherRepos
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VoucherRepos(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task create(Voucher v)
+        {
+            try
+            {
+               await _context.Accounts.AddAsync(v);
+                await _context.SaveChangesAsync();
+            }catch (Exception ex)
+            {
+
+            }
+        }
+
+        public Task delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Voucher>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Voucher> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task update(Voucher voucher)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
