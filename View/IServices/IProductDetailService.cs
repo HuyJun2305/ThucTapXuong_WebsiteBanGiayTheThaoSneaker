e56b03c80f9cd4755232c77a823ec9acb4e8c659
HuyJun2305 <125681217+HuyJun2305@users.noreply.github.com>
2024-09-27 10:31:00 +0700

    Service
    Service and IService

---
+++ View/IServices/IProductDetailService.cs
@@ -0,0 +1,15 @@
+﻿using DataProcessing.Models;
+
+namespace View.IServices
+{
+    public interface IProductDetailService
+    {
+        Task<List<ProductDetail>> GetAllProductDetail();
+        Task<ProductDetail> GetProductDetailById(Guid id);
+        Task Create(ProductDetail productDetail);
+        Task Update(ProductDetail productDetail);
+        Task Delete(Guid id);
+
+        Task SaveChanges();
+    }
+}
