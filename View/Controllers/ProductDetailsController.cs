de80cc2f778b0d36207ac4ca1d50a655b74e7102
HuyJun2305 <125681217+HuyJun2305@users.noreply.github.com>
2024-09-27 13:33:57 +0700

    Create ProductDetailsController.cs

---
+++ View/Controllers/ProductDetailsController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using View.IServices;
+
+namespace View.Controllers
+{
+    public class ProductDetailsController : Controller
+    {
+        private readonly IProductServices _productServices;
+        private readonly ISoleServices _soleServices;
+        private readonly ICategoryServices _categoryServices;
+        private readonly IBrandServices _brandServices;
+        private readonly IMaterialServices _materialServices;
+        private readonly IProductDetailService _productDetailService;
+        private readonly ISizeServices _sizeServices;
+        private readonly IColorServices _colorServices;
+
+
+        // GET: ProductDetailsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ProductDetailsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductDetailsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductDetailsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductDetailsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductDetailsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductDetailsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductDetailsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
