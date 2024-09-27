5287b1a11e6bc9821a66513651e1f13fc628c5a5
thanhdz2003 <115550700+thanhdz2003@users.noreply.github.com>
2024-09-27 14:17:30 +0700

    fix

--- API/Program.cs
+++ API/Program.cs
@@ -28,11 +28,16 @@ builder.Services.AddScoped<ISoleRepo, SoleRepos>();
 builder.Services.AddScoped<ICategoryRepo, CategoryRepos>();
 builder.Services.AddScoped<IBrandRepo, BrandRepos>();
 builder.Services.AddScoped<IMaterialRepo, MaterialRepos>();
+//builder.Services.AddScoped<IColorRepo, ColorRepo>();
+//builder.Services.AddScoped<ISizeRepo, SizeRepo>();
+//builder.Services.AddScoped<IImageRepo, ImageRepo>();
+//builder.Services.AddScoped<ISelectedImageRepo, SelectedImageRepo>();
 builder.Services.AddScoped<IPromotionRepos, PromotionRepos>();
 builder.Services.AddScoped<IProductDetailPromotionRepos, ProductDetailPromotionRepo>();
 builder.Services.AddScoped<IColorRepo, ColorRepo>();
 builder.Services.AddScoped<IImageRepo, ImageRepo>();
 builder.Services.AddScoped<ISelectedImageRepo, SelectedImageRepo>();
+
 
 builder.Services.AddAuthentication(options =>
 {
