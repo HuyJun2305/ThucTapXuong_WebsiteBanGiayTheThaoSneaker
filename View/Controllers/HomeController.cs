b0d3c9baf09e237207b17a963d2b38ca217d82c0
HuyJun2305 <125681217+HuyJun2305@users.noreply.github.com>
2024-10-11 17:31:27 +0700

    demo home client

--- View/Controllers/HomeController.cs
+++ View/Controllers/HomeController.cs
@@ -1,22 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
 using View.IServices;
 using View.Models;
+using View.Servicecs;
 
 namespace View.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IProductServices productServices;
+        private readonly IProductServices _productServices;
+        private readonly ISizeServices _sizeServices;
+        private readonly IColorServices _colorServices;
+        private readonly ICategoryServices _categoryServices;
 
-        public HomeController(IProductServices _productServices)
+
+        public HomeController(IProductServices productServices, ISizeServices sizeServices,
+            IColorServices colorServices,
+            ICategoryServices categoryServices)
         {
-            productServices = _productServices;
+            _productServices = productServices;
+            _sizeServices = sizeServices;
+            _colorServices = colorServices;
+            _categoryServices = categoryServices;
         }
 
         public async Task<IActionResult> Index()
         {
-            var viewContext = productServices.GetAllProducts().Result;
+            ViewData["ColorId"] = new SelectList((await _colorServices.GetAllColors()).Where(x => x.Status), "Id", "HEX");
+            ViewData["SizeId"] = new SelectList((await _sizeServices.GetAllSizes()).Where(x => x.Status), "Id", "Value");
+            ViewData["ProductId"] = new SelectList(await _productServices.GetAllProducts(), "Id", "Name");
+            ViewData["CategoryId"] = new SelectList(_categoryServices.GetAllCategories().Result.Where(x => x.Status), "Id", "Name");
+
+            var viewContext = _productServices.GetAllProducts().Result;
             if (viewContext == null) return View("'Product is null!'");
             return View(viewContext.ToList());
         }
