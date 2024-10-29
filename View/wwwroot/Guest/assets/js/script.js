64bdadfe1ac2e9022addda5874dbfd2ba020e051
HuyJun2305 <125681217+HuyJun2305@users.noreply.github.com>
2024-10-30 00:32:11 +0700

    delete fe

--- View/wwwroot/Guest/assets/js/script.js
+++ View/wwwroot/Guest/assets/js/script.js
@@ -40,38 +40,3 @@ window.addEventListener("scroll", function () {
 });
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    const navOpenBtn = document.querySelector('[data-nav-open-btn]');
-    const navCloseBtn = document.querySelector('[data-nav-close-btn]');
-    const navbar = document.querySelector('[data-navbar]');
-    const overlay = document.querySelector('[data-overlay]');
-    const searchBtn = document.querySelector('.search-btn');
-    const searchInput = document.querySelector('.search-input');
-
-    navOpenBtn.addEventListener('click', () => {
-        navbar.classList.add('active');
-        overlay.classList.add('active');
-    });
-
-    navCloseBtn.addEventListener('click', () => {
-        navbar.classList.remove('active');
-        overlay.classList.remove('active');
-    });
-
-    overlay.addEventListener('click', () => {
-        navbar.classList.remove('active');
-        overlay.classList.remove('active');
-    });
-
-    searchBtn.addEventListener('click', () => {
-        searchInput.classList.toggle('active');
-        const inputField = searchInput.querySelector('input');
-        inputField.focus();
-    });
-
-    document.addEventListener('click', (event) => {
-        if (!searchInput.contains(event.target) && !searchBtn.contains(event.target)) {
-            searchInput.classList.remove('active');
-        }
-    });
-});
